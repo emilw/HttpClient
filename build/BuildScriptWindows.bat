48328a799f51e008f5d16cd20c37aa2175f8ef36
unknown <emilw@konsult47.medius.local>
2015-01-02 18:20:53 +0100

    Settings file update for Travis

--- build/BuildScriptWindows.bat
+++ build/BuildScriptWindows.bat
@@ -1,1 +1,10 @@
- C:\Chocolatey\bin\cmake.bat "Visual Studio 11 2012" ..
+C:\Chocolatey\bin\cmake.bat "Visual Studio 11 2012" ..
+Import-Module -Name ".\WindowsAdditions\Invoke-MsBuild.psm1"
+
+Write-Host("Starting build")
+$buildSucceeded = Invoke-MsBuild -Path ".\HttpClient.sln"
+
+if ($buildSucceeded)
+{ Write-Host "Build completed successfully." }
+else
+{ Write-Host "Build failed. Check the build log file for errors." }
